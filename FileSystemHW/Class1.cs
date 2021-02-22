8f639f8f420096d477343d8fccfc22bccdc6d8dd
Bola_sw25@student.itstep.kz <Bola_sw25@student.itstep.kz>
2021-02-22 10:40:42 +0600

    Добавьте файлы проекта.

---
+++ FileSystemHW/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FileSystemHW
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Visual Studio 2019\Code Snippets\Visual C#\My Code Snippets\";
+            string[] words;
+            List<int> numbers = new List<int>();
+            using (StreamReader stream = new StreamReader(path + "input.txt"))
+            {
+                string text = stream.ReadLine();
+                words = text.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+            }
+            bool isInt = false;
+            foreach (string item in words)
+            {
+                int number;
+                isInt = int.TryParse(item, out number);
+                if (isInt)
+                    numbers.Add(number);
+            }
+            int result = numbers[0] + numbers[1];
+            Console.WriteLine(numbers[0] + " + " + numbers[1] + " = " + result);
+            using (FileStream stream = new FileStream(path + "output.txt", FileMode.Create))
+            {
+                string data = result.ToString();
+                byte[] bytes = System.Text.Encoding.Unicode.GetBytes(data);
+                stream.Write(bytes, 0, bytes.Length);
+            }
+            Console.ReadLine();
+        }
+    }
+}
